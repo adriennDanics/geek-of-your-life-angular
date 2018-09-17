247c9d540dd7bc291492a510b1befc5d358d779a
orsolyaLevai <solya0082@gmail.com>
2018-09-17 13:08:15 +0200

    Added components

---
+++ src/app/userhandling/userhandling.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userhandling',
+  templateUrl: './userhandling.component.html',
+  styleUrls: ['./userhandling.component.css']
+})
+export class UserhandlingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
