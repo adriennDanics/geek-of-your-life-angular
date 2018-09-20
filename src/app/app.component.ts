2669284e4277a632fed838ff13ef82101f591b7c
orsolyaLevai <solya0082@gmail.com>
2018-09-20 17:32:23 +0200

    Listing the users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,38 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IUser } from './user';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  /* template: 
+    `<h2>Users</h2>
+    <ul *ngFor= "let user of users">
+      <li>{{ user.email }}</li>
+    </ul>
+    ` */
 })
 
 export class AppComponent {
   title = 'geek-of-your-life';
   user: boolean = false;
 
+  public users = [];
+
   constructor(private http: HttpClient) {
 
   }
 
   ngOnInit() {
-    let obs = this.http.get('http://localhost:8080/users/');
-    obs.subscribe((response) => console.log(response))
+    //let obs = this.http.get('http://localhost:8080/users/');
+    //obs.subscribe((response) => console.log(response))
+    this.getUsers().subscribe(data => this.users = data);
+    
+  }
+
+  getUsers():Observable<IUser[]> {
+    return this.http.get<IUser[]>('http://localhost:8080/users/')
   }
 }
